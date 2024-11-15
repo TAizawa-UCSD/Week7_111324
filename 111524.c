e07d19c09894cbee2871522d7be26fcc80ce46d1
TAizawa-UCSD <taaizawa@ucsd.edu>
2024-11-15 10:14:13 -0800

    Update 111524.c

--- 111524.c
+++ 111524.c
@@ -1,6 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <sys/mean.h>
+#include <sys/mman.h>
 
-#define HEAP_START 400
+#define HEAP_SIZE 400
+#define VAL_SIZE 8
+
+uint64_t* HEAP_START = NULL;
+
+void init_heap(){
+  //Hey operating system, where can I start my heap?
+  
+  uint64_t *heap = mmap(NULL, HEAP_SIZE, PROT_READ | PROT_WRITE, MAO_ANON | MAP_SHARED, -1, 0);//mmap means "Hey OS, I want heap memory, give me a bunch and tell me where it started
+  HEAP_START = heap;
+}
+
+void* my_malloc(size_t size){
+  sdf
+}
+
+int main(){
+  int* a = my_malloc(40);
+  int* b = my_malloc(10);
+}
